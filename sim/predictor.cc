12d6e0d9b6b983d31546fa2792f37410ca07bdda
Leo Lu <lyuholu@gmail.com>
2024-11-18 14:25:17 -0500

    feat: implement constructor

--- sim/predictor.cc
+++ sim/predictor.cc
@@ -1,7 +1,20 @@
 #include "predictor.h"
 
+// Total predictor storage should not exceed 128KB.
+
 PREDICTOR::PREDICTOR(void)
 {
+  ghr = bitset<HIST_LEN>();
+
+  for (int i = 0; i < TABLE_LEN; i++)
+  {
+    for (int j = 0; j < HIST_LEN; j++)
+    {
+      table[i][j] = 0;
+    }
+  }
+
+  y = 0;
 }
 
 bool PREDICTOR::GetPrediction(UINT32 PC)
@@ -19,4 +32,9 @@ void PREDICTOR::TrackOtherInst(UINT32 PC, OpType opType, UINT32 branchTarget)
   // conditional branches, just in case someone decides to design
   // a predictor that uses information from such instructions.
   // We expect most contestants to leave this function untouched.
+}
+
+UINT32 PREDICTOR::Hash(UINT32 PC)
+{
+  return 0; // tmp
 }
